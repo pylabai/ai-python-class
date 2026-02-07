bef946204fa9286b9433dbf9763905b88187ad2c
pylabai <onokmic@gmail.com>
2026-02-07 11:01:40 +0800

    Update Canny Edge Finding.py

--- Canny Edge Finding.py
+++ Canny Edge Finding.py
@@ -6,22 +6,22 @@ Created on Thu Nov 18 14:10:02 2021
 """
 import cv2
 
-# 讀取原始影像 [cite: 200]
+# 讀取原始影像
 img = cv2.imread('pepper.jpg', -1)
-cv2.imshow('Original', img) [cite: 201]
+cv2.imshow('Original', img)
 cv2.waitKey(0)
 
-# 讀取為灰階影像 [cite: 206]
+# 讀取為灰階影像
 img_grey = cv2.imread('pepper.jpg', 0)
-cv2.imshow('Greyscale', img_grey) [cite: 205]
+cv2.imshow('Greyscale', img_grey)
 cv2.waitKey(0)
 
 # 執行 Canny 邊緣偵測 
 # threshold1 與 threshold2 設定為 120
 edges = cv2.Canny(image=img_grey, threshold1=120, threshold2=120)
 
-# 顯示結果 [cite: 213]
+# 顯示結果
 cv2.imshow('Canny Edge Detection', edges)
 cv2.waitKey(0)
 
-cv2.destroyAllWindows() [cite: 216]
+cv2.destroyAllWindows()
